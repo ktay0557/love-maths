65dc1984207977f6709c885cf73c3dd3f1533c04
Jade <ktay0557@gmail.com>
2024-01-14 15:44:52 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+//runGame
+function runGame() {
+
+}
+
+//checkAnswer
+function checkAnswer() {
+
+}
+
+//calculateCorrectAnswer
+function calculateCorrectAnswer() {
+
+}
+
+//incrementScore
+function incrementScore() {
+
+}
+
+//incrementWrongAnswer
+function incrementWrongAnswer() {
+
+}
+
+//displayAdditionQuestion
+function displayAdditionQuestion() {
+
+}
+
+//displaySubtractQuestion
+function displaySubtractQuestion() {
+    
+}
+
+//displayMultiplyQuestion
+function displayMultiplyQuestion() {
+    
+}
+
+//displayDivisionQuestion
+function displayDivisionQuestion() {
+    
+}
